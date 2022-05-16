3a77bd100c70d81d4ef7f9296b6ad36428b913bb
William Falcon <waf2107@columbia.edu>
2022-05-15 22:24:42 -0400

    build slack component

--- tests/test_slack_component.py
+++ tests/test_slack_component.py
@@ -1,4 +1,3 @@
-import imp
 import os
 from slack.component import SlackMessenger
 
@@ -6,8 +5,6 @@ from slack.component import SlackMessenger
 def test_send_message():
     token = os.environ['SLACK_TOKEN']
     channel_id = os.environ['SLACK_CHANNEL_ID']
-    print(token)
-    print(channel_id)
 
     messenger = SlackMessenger(token=token, channel_id=channel_id)
     messenger.run('test from CI/CD')
