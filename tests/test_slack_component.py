92dbc84e3b2a05383cf9c9c22e3770cb0d19f407
William Falcon <waf2107@columbia.edu>
2022-06-05 21:30:27 -0400

    change syntax

--- tests/test_slack_component.py
+++ tests/test_slack_component.py
@@ -7,5 +7,5 @@ def test_send_message():
     channel_id = os.environ['SLACK_CHANNEL_ID']
 
     messenger = SlackMessenger(token=token, channel_id=channel_id)
-    result = messenger.run('test from CI/CD')
+    result = messenger.send_message('test from CI/CD')
     assert result.status_code == 200
