0bec51882d12ac01c0b8944e640f7993ecb2f2f4
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2023-02-13 17:15:59 +0900

    setup install dependencies (#11)

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
+import os
 
+from pkg_resources import parse_requirements
 from setuptools import setup, find_packages
+
+
+_PATH_ROOT = os.path.dirname(__file__)
+
+
+def _load_requirements(path_dir: str = _PATH_ROOT, file_name: str = "requirements.txt") -> list:
+    reqs = parse_requirements(open(os.path.join(path_dir, file_name)).readlines())
+    return list(map(str, reqs))
+
 
 setup(
     name='lit_slack',
@@ -10,6 +21,6 @@ setup(
     author_email='',
     # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
     url='https://github.com/PyTorchLightning/lightning-component-template',
-    install_requires=[],
-    packages=find_packages(),
+    install_requires=_load_requirements(),
+    packages=find_packages(exclude="tests"),
 )
