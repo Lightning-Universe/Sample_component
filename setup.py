8814165663ad6bfb07cf6ac9f773aa22852801fa
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2023-02-16 03:47:31 +0900

    installable package & ci (#13)

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+from importlib.util import module_from_spec, spec_from_file_location
 
 from pkg_resources import parse_requirements
 from setuptools import find_packages, setup
@@ -7,19 +8,34 @@ from setuptools import find_packages, setup
 _PATH_ROOT = os.path.dirname(__file__)
 
 
+def _load_py_module(fname: str, pkg: str = "lit_slack"):
+    spec = spec_from_file_location(os.path.join(pkg, fname), os.path.join(_PATH_ROOT, pkg, fname))
+    py = module_from_spec(spec)
+    spec.loader.exec_module(py)
+    return py
+
+
 def _load_requirements(path_dir: str = _PATH_ROOT, file_name: str = "requirements.txt") -> list:
     reqs = parse_requirements(open(os.path.join(path_dir, file_name)).readlines())
     return list(map(str, reqs))
 
 
+about = _load_py_module("__about__.py")
+with open(os.path.join(_PATH_ROOT, "README.md"), encoding="utf-8") as fo:
+    readme = fo.read()
+
+
 setup(
     name="lit-slack",
-    version="0.1.0",
-    description="Describe Your Cool Component",
-    author="",
-    author_email="",
+    version=about.__version__,
+    description=about.__docs__,
+    author=about.__author__,
+    author_email=about.__author_email__,
+    license=about.__license__,
     # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
-    url="https://github.com/PyTorchLightning/lightning-component-template",
+    url=about.__homepage__,
+    long_description=readme,
+    long_description_content_type="text/markdown",
     install_requires=_load_requirements(),
     packages=find_packages(exclude=["tests", "tests.*"]),
 )
