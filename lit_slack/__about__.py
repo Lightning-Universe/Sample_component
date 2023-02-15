08884ca8494e3f9186827ea76cfd87e06f076261
Jirka <jirka.borovec@seznam.cz>
2023-02-15 19:35:50 +0100

    about

---
+++ lit_slack/__about__.py
@@ -0,0 +1,18 @@
+__version__ = "0.1.0"
+__author__ = "Lightning et al."
+__author_email__ = "name@lightning.ai"
+__license__ = "Apache 2.0"
+__copyright__ = f"Copyright (c) 2021-2023, {__author__}."
+__homepage__ = "https://github.com/Lightning-Universe/Sample_component"
+__docs__ = "Describe Your Cool Component"
+
+
+__all__ = [
+    "__author__",
+    "__author_email__",
+    "__copyright__",
+    "__docs__",
+    "__homepage__",
+    "__license__",
+    "__version__",
+]
--- lit_slack/__init__.py
+++ lit_slack/__init__.py
@@ -1,3 +1,4 @@
+from lit_slack.__about__ import *  # noqa: F403
 from lit_slack.component import SlackMessenger
 
 __all__ = ["SlackMessenger"]
